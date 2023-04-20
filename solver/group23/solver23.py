ab4415e508e3902d5809199a5038a03e21f1ca7c
Alessandro Redi <alessandroredi1@gmail.com>
2023-04-20 18:24:20 +0200

    Class solver23

--- solver/group23/solver23.py
+++ solver/group23/solver23.py
@@ -2,3 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # try commit with collaborator
+
+class Solver23():
+    def __init__(self):
+        """
+        Solver: 
+        """
+        self.name = "solver23"
+
+
+    
