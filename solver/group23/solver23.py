da63fb058a4e7b6566c88ac3206e6c2a1dae4a3f
federicovolponi <federicovolponi99@gmail.com>
2023-04-30 18:50:13 +0200

    trail update,  popStack widtwise
    Co-authored-by: Alessandro Redi <AlessandroRedi@users.noreply.github.com>

--- solver/group23/solver23.py
+++ solver/group23/solver23.py
@@ -3,13 +3,18 @@
 import os
 import pandas as pd
 import numpy as np
-from numpy.random import choice
 from sub.stack import Stack
 from sub.utilities import *
+from sub.ACO import ACO
 #TODO:
-# - in addStack boolean quando camion pieno
-# - integrare classe ACO con solver
-# - 
+# - in popStack pensare a cosa fare se non ci sono più stack con quel
+#   stack_code
+# - aggiornamento pr_move (controllare che somma delle probabilite su
+#   sia uguale ad 1)
+# - creare matrice attractiveness
+# - pensare a come inizializzare pr_move in modo che piaccia a fox
+# - creare gli output in modo da avere in stampa le immagini del camion
+# - evaluate  ant.weigthSolution: trovare un  modo per valutare la bontà della soluzione
 class Solver23():
     def __init__(self):
         """
@@ -20,29 +25,31 @@ class Solver23():
     def solve_single_vehicle(self, df_items, df_vehicles):
         """ 
         solve_single_vehicle
-        -----
-        test function to work with ACO on the 2D bin packing of
-        a single vehicle
+        --------------------
+
+        test function to work with ACO on the 2D bin packing of a single vehicle
         """
         self.df_items = df_items
         self.df_vehicles = df_vehicles
-
-        self.pr_move = statesCreation(df_items[["stackability_code","forced_orientation"]].drop_duplicates())
         
         # for on truck's type
         # work on single truck
         vehicle = self.df_vehicles.iloc[0] # vehicle type V0
         
         stack_lst = self.buildStacks(vehicle)
-
-        self.ACO_2D_bin(stack_lst, vehicle,alpha = 1, beta = 1, n_ants = 10)
+        aco = ACO(stack_lst, vehicle)
+        aco.pr_move = statesCreation(df_items[["stackability_code",
+                                               "forced_orientation"]].drop_duplicates())
+        aco.aco_2D_bin()
+        
 
     def buildStacks(self, vehicle):
         """"
         buildStacks
         -----------
-        vehicle: vehicle type, needed to check the height for
-                 creating the stacks for this specific truck
+
+        - vehicle: vehicle type, needed to check the height for
+                   creating the stacks for this specific truck
         """
         
         # stack creation: adesso è fatta in modo molto stupido ma dato che item con lo stesso
@@ -83,99 +90,15 @@ class Solver23():
                     
         return stack_lst
         
-    def ACO_2D_bin(self, stack_lst, vehicle, alpha = 1, beta = 1, n_ants = 10):
-        """ 
-        Ant Colony Optimization for 2D bin packing
-        ---
-        #### INPUT PARAMETERS:
-            - stack_lst: list of stacks (format: [[I001, I002, NaN], [I003, I005, NaN]])
-            - alpha: realative trail importance (default 1)
-            - beta: relative attractivness importance (default 1)
-            - n_ants: number of ants
-        #### ACO PARAMETERS:
-            - attr(η): matrix of attractiveness from state i to j
-            - trail(τ): matrix of trails from state i to j
-            - pr_move: 8x8 matrix of probabilities of moves from i to j (state 7 is related to empty vehicle)
-        """
-        attr = np.random.rand(6, 6) #NOTE: take random value for attractivenss matrix just to test
-        # initialize pr_move with same prob for each movement but set to zero prob to move to no stack
-        pr_move = np.full((8,8), 1./7) * np.array([1, 1, 1, 1, 1, 1, 1, 0]) #TODO: create automatically the matrix
-        #TODO: outer loop contaning a termination condition (no. of iterations, solution's goodness???)
-        ants = []
-        for k in range(n_ants):
-            free_space = True 
-            prev_s_code = 6 # empty vehicle state
-            # initialize solution of ant k
-            sol = {
-            "type_vehicle": [],
-            "idx_vehicle": [],
-            "id_stack": [],
-            "id_item": [],
-            "x_origin": [],
-            "y_origin": [],
-            "z_origin": [],
-            "orient": []
-            }
-            x_pos = 0
-            y_pos = y_max = 0
-            ant_k = []
-            while(free_space):  # loop until free space available in vehicle
-                next_s_code = self.choose_move(pr_move, prev_s_code) 
-                # ants[ant_k].append()
-                new_stack, stack_lst = popStack(stack_lst, next_s_code) #TODO: what if no more stacks with this stack code??
-                #TODO: se widthwise mettere come length la width
-                #TODO: posizionare new_stack
-                toAddStack, x_pos, y_max = self.addStack(new_stack, x_pos, y_pos, y_max, vehicle)
-                ant_k.append(toAddStack)
-                prev_s_code = next_s_code
-                #TODO: controllo se free space
-                
-    def choose_move(self, pr_move, prev_s_code):
-        """ 
-        choose_move
-        -----
-        #### INPUT PARAMETERS:
-            - pr_move: matrix of probabilities of moves from i to j 
-            - prev_s_code: stackability code of the last stack placed into the truck
-        #### OUTPUT PARAMETERS:
-            - next_s_code: stackability code of the next stack to be placed into the truck
-        """
-        row_to_choose = pr_move[prev_s_code][:] # select the row from the stack the ant is moving
-        next_s_code = int(choice(range(len(row_to_choose)), p=row_to_choose))
-        
-        return next_s_code 
-    
-    def addStack(self, toAddStack, x_pos, y_pos, y_max, vehicle):
-        """  
-        
-        """
-        if x_pos + toAddStack.length < vehicle['length']:
-            toAddStack.position(x_pos, y_pos)
-            x_pos += toAddStack.length
-            if toAddStack.width > y_max:
-                y_max = toAddStack.width
-        else:
-            x_pos = 0
-            y_pos = y_max
-            y_max = 0
-            if y_pos + toAddStack.width < vehicle['width']:
-                toAddStack.position(x_pos, y_pos)
-                x_pos += toAddStack.length
-                if toAddStack.width > y_max:
-                    y_max = toAddStack.width
-            else:
-                # completed solution
-                pass
-        return toAddStack, x_pos, y_max
                 
     def solve(self, df_items, df_vehicles):
         """ 
         solve
         -----
-        df_items: dataframe containing all the items 
-                  that are to be put into the trucks
-        df_vehicles: dataframe containing all the different
-                     types of trucks that can be choose
+        - df_items: dataframe containing all the items 
+                    that are to be put into the trucks
+        - df_vehicles: dataframe containing all the different
+                       types of trucks that can be choose
         """
         #TODO: take random 200 items for less computation (random for getting different stack code)
         self.solve_single_vehicle(df_items, df_vehicles)
