a355299954ed45c45658e457e931e739f03b9bc8
federicovolponi <federicovolponi99@gmail.com>
2023-10-21 18:35:17 +0200

    trucks integration

--- benchmark/model_col_gen.py
+++ benchmark/model_col_gen.py
@@ -8,6 +8,7 @@ from aco.aco_bin_packing import ACO
 from aco.utilities import buildStacks, stackInfo_creation
 import time
 
+# RUN python benchmark/model_col_gen.py
 class model_col_gen:
     
     def __init__(self, df_vehicles, df_items):
@@ -52,6 +53,8 @@ class model_col_gen:
             id_truck = self.df_vehicles.iloc[i]["id_truck"]
             self.stack_lst[id_truck], self.stack_quantity[id_truck] = buildStacks(self.df_vehicles.iloc[i], self.df_items, self.stackInfo)
             print(f"\n Stack for vehicle {id_truck} created")
+
+            
         
 
     def add_columns(self, n_cols, duals, vehicle):
@@ -95,20 +98,27 @@ class model_col_gen:
         model = gp.Model("ColumnGeneration")       
         
         columns = np.zeros([self.items_type, N_COLS])
+        # Dict to map the column index to the vehicle ID
         cols_to_vehicle = {}
-        duals = np.zeros([self.items_type, 1])
+
+        # Initialize duals associated to a specific vehicle        
+        duals_dict = {}
+        for _ in range(self.n_vehicles):
+            id_truck = self.df_vehicles.iloc[_]["id_truck"]
+            duals_dict[id_truck] = np.zeros([self.items_type, 1])
 
         t_start = time.time()
         ind_truck = 0
         n_iter = 100 #TODO: set in config
         _iter = 0
         while (time.time() - t_start) <= TIME_LIMIT and _iter <= n_iter * N_COLS:
-            x = model.addVars(_iter + N_COLS, 1, vtype=GRB.CONTINUOUS, name="x")
+            x = model.addVars(_iter + N_COLS, vtype=GRB.CONTINUOUS, lb = 0, name="x")
 
             # Create new columns from the ACO solution
             vehicle = self.df_vehicles.iloc[ind_truck].to_dict()
+            duals = duals_dict[vehicle["id_truck"]]
             columns[:,_iter:_iter+N_COLS] = self.add_columns(n_cols = N_COLS, duals = duals, vehicle = vehicle)
-
+            
             # Map columns index with vehicle ID
             for i in range(_iter, columns.shape[1]):
                 cols_to_vehicle[i] = vehicle["id_truck"] 
@@ -117,7 +127,7 @@ class model_col_gen:
             for i in range(self.items_type):
                 constr_i = 0
                 for n in range(columns.shape[1]):
-                    constr_i += columns[i, n] * x[n, 0]
+                    constr_i += columns[i, n] * x[n]
 
                 model.addConstr(
                     constr_i >= self.n_items_type[i],
@@ -128,13 +138,15 @@ class model_col_gen:
 
             obj = gp.LinExpr()
             
-            for n in range(columns.shape[1]):
-                cost = self.df_vehicles[self.df_vehicles["id_truck"] == cols_to_vehicle[n]]["cost"].iloc[0]
-                obj += cost * x[n, 0]   #TODO: replace with gp.quicksum()?
+            for n in range(_iter, columns.shape[1]):
+                #cost = self.df_vehicles[self.df_vehicles["id_truck"] == cols_to_vehicle[n]]["cost"].iloc[0]
+                obj += vehicle["cost"] * x[n]
 
             model.setObjective(obj, GRB.MINIMIZE)
-
+            model.write(f"./benchmark/results/model_{_iter}.lp")
+            model.update()
             model.optimize()
+            print([x[n].X for n in range(_iter + N_COLS)])
 
             columns = np.append(columns, np.zeros([self.items_type, N_COLS]), axis=1)
 
@@ -149,10 +161,11 @@ class model_col_gen:
                 cons = model.getConstrs()
                 
                 for i in range(self.items_type): 
-                        duals[i] = cons[i].getAttr('Pi')
+                    duals_dict[vehicle["id_truck"]][i] = cons[i].getAttr('Pi')
 
+                
                 # Print the values of the dual variables
-                if PRINT:
+                if PRINT_CG:
                     print("\nDUAL VARIABLES")
                     for i in range(self.items_type): 
                         print(f"The dual of constr {i} is :{cons[i].getAttr('Pi')}")
