478c70ce824172a92b464c169560e7ec4e00b285
davmacario <dav.macario@gmail.com>
2023-10-21 00:16:14 -0500

    Add loading bar function

--- solver/group23/sub/utilities.py
+++ solver/group23/sub/utilities.py
@@ -122,3 +122,22 @@ def map_items(df_items, stackInfo_App):
     return df_items
 
 
+def loadingBar(current_iter: int, tot_iter: int, n_chars: int=10, ch: str="=", n_ch: str=" ") -> str:
+    """ 
+    loadingBar
+    ---
+    Produce a loading bar string to be printed.
+
+    ### Input parameters
+    - current_iter: current iteration, will determine the position
+    of the current bar
+    - tot_iter: total number of iterations to be performed
+    - n_chars: total length of the loading bar in characters
+    - ch: character that makes up the loading bar (default: =)
+    - n_ch: character that makes up the remaining part of the bar 
+    (default: blankspace)
+    """
+    n_elem = int(current_iter * n_chars / tot_iter)
+    prog = str("".join([ch] * n_elem))
+    n_prog = str("".join([" "] * (n_chars - n_elem - 1)))
+    return "[" + prog + n_prog + "]"
