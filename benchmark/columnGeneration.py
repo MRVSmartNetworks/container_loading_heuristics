8fead702b9c6f77eabe035b1f8af57a4d7ddcf96
davmacario <dav.macario@gmail.com>
2023-10-29 10:52:57 -0500

    Format column generation <3

--- benchmark/columnGeneration.py
+++ benchmark/columnGeneration.py
@@ -5,12 +5,14 @@ import time
 import random
 from masterProblem import MasterProblem
 from sub.utilities import stackInfo_creation, buildStacks
+from sub.aco_bin_packing_slices import ACO
 from sub.aco_bin_packing import ACO
 from sub.config import ALPHA, BETA, N_ANTS, N_ITER
 from configCG import N_INIT_COLS, N_COLS, TIME_LIMIT
 
+
 class columnGeneration:
-    """  
+    """
     columnGeneration
     -----------------------------------------
     Implementation of column generation using as subproblem the ACO
@@ -26,21 +28,21 @@ class columnGeneration:
     - generateColumns
     - genereateInitialColumns
     """
+
     def __init__(self, df_vehicles, df_items):
         self.df_vehicles = df_vehicles
         self.df_items, self.stackInfo = stackInfo_creation(df_items)
         self.items_type = len(self.stackInfo)
         self.n_vehicles = len(self.df_vehicles)
-        
+
         # Evaluate the number of items per type in df_items
         self.n_items_type = np.zeros(self.items_type)
         for i in range(self.items_type):
-            self.n_items_type[i] =  len(df_items.loc[df_items['stackability_code'] == i])
-        
+            self.n_items_type[i] = len(df_items.loc[df_items["stackability_code"] == i])
+
         # Ant Colony Optimizazion initialization
         self.aco = ACO(
-            self.stackInfo, alpha=ALPHA, beta=BETA, 
-            n_ants=N_ANTS, n_iter=N_ITER
+            self.stackInfo, alpha=ALPHA, beta=BETA, n_ants=N_ANTS, n_iter=N_ITER
         )
 
         # Creation of the stacks for each vehicle
@@ -51,45 +53,48 @@ class columnGeneration:
         print("BUILDING STACKS FOR EACH VEHICLE\n")
         for i in range(self.n_vehicles):
             id_truck = self.df_vehicles.iloc[i]["id_truck"]
-            self.stack_lst[id_truck], self.stack_quantity[id_truck] = buildStacks(self.df_vehicles.iloc[i], self.df_items, self.stackInfo)
+            self.stack_lst[id_truck], self.stack_quantity[id_truck] = buildStacks(
+                self.df_vehicles.iloc[i], self.df_items, self.stackInfo
+            )
             print(f"Stack for vehicle {id_truck} created")
         print(f"\nElapsed time to create stacks: {round(time.time() - t_start, 2)} s")
 
-    def solve(self, sol_file_name = None):
+    def solve(self, sol_file_name=None):
         t_start = time.time()
         # Generate initial columns for each vehicle
-        initColumns = self.generateInitialColumns(n_cols = N_INIT_COLS)
+        initColumns = self.generateInitialColumns(n_cols=N_INIT_COLS)
 
         # Definition of the master problem and build of the model
         master = MasterProblem(initColumns, self.df_vehicles, self.df_items)
-        
+
         _iter = 0
         duals = 0
         t_start = time.time()
-        while  (time.time() - t_start) <= TIME_LIMIT:
+        while (time.time() - t_start) <= TIME_LIMIT:
             # Solve relaxed master
             objVal = master.solveRelaxedModel(_iter)
             if objVal:
                 duals = master.getDuals()
 
             # FIXME: Dummy solution
-            vehicle = self.df_vehicles.iloc[random.randint(0, self.n_vehicles)-1].to_dict()
-            
+            vehicle = self.df_vehicles.iloc[
+                random.randint(0, self.n_vehicles) - 1
+            ].to_dict()
+
             # Solve the Ant Colony optimization
-            newColumns = self.generateColumns(n_cols = N_COLS,
-                                              duals = duals,
-                                              vehicle = vehicle)
-            
+            newColumns = self.generateColumns(
+                n_cols=N_COLS, duals=duals, vehicle=vehicle
+            )
+
             # Add new columns to master
             master.addColumns(vehicle["cost"], newColumns)
             _iter += 1
-        
-        master.solveModel(file_name = "benchmark/results/model.lp")
+
+        master.solveModel(file_name="benchmark/results/model.lp")
         print(f"\nTotal elapsed time: {round(time.time() - t_start, 2)} s")
-            
 
     def generateColumns(self, n_cols, duals, vehicle):
-        """  
+        """
         generateColumns
         ---------------------------------------------
         Method to generate a given number of columns for a given
@@ -99,22 +104,25 @@ class columnGeneration:
         columns = np.zeros((self.items_type, n_cols))
         # Give a vehicle to ACO and the specific stack list
         self.aco.getVehicle(vehicle)
-        self.aco.getStacks(self.stack_lst[vehicle["id_truck"]], self.stack_quantity[vehicle["id_truck"]])
+        self.aco.getStacks(
+            self.stack_lst[vehicle["id_truck"]],
+            self.stack_quantity[vehicle["id_truck"]],
+        )
 
         # Solve the 2D bin packing problem
-        bestAnts = self.aco.aco_2D_bin(n_bestAnts = n_cols, dualVars = duals)
+        bestAnts = self.aco.aco_2D_bin(n_bestAnts=n_cols, dualVars=duals)
         for j, ant in enumerate(bestAnts):
             for stack in ant:
-                #TODO: use code to state
+                # TODO: use code to state
                 columns[stack.stack_code][j] += len(stack.items)
-        
+
         return columns
-    
+
     def generateInitialColumns(self, n_cols):
-        """  
+        """
         generateInitialColumns
         ---------------------------------------------
-        Method to generate the initial set of columns for each type 
+        Method to generate the initial set of columns for each type
         of vehicle in df_vehicles. The columns are generated using
         the ACO.
         """
@@ -124,14 +132,16 @@ class columnGeneration:
         _iter = 0
         for i in range(self.n_vehicles):
             vehicle = self.df_vehicles.iloc[i].to_dict()
-            columns_vehicle = self.generateColumns(n_cols = N_INIT_COLS,
-                                                   duals = 0,
-                                                   vehicle = vehicle)
+            columns_vehicle = self.generateColumns(
+                n_cols=N_INIT_COLS, duals=0, vehicle=vehicle
+            )
             print(f"Solution found for vehicle {vehicle['id_truck']}")
 
-            columns[:, _iter:_iter+N_INIT_COLS] = columns_vehicle
+            columns[:, _iter : _iter + N_INIT_COLS] = columns_vehicle
             _iter += N_INIT_COLS
-        
-        print(f"\nElapsed time to create initial columns: "
-              f"{round(time.time() - t_start, 2)} s")
+
+        print(
+            f"\nElapsed time to create initial columns: "
+            f"{round(time.time() - t_start, 2)} s"
+        )
         return columns
