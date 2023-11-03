d1e5af8eb43a14de592f928e2da444f37da4a1c6
federicovolponi <federicovolponi99@gmail.com>
2023-11-03 19:06:25 +0100

    master with ACO cols

--- benchmark/solver_ACO.py
+++ benchmark/solver_ACO.py
@@ -103,7 +103,7 @@ class SolverACO:
                 for _ in range(PARAM_ITER):
                     print(f"Iteration number : {test_ind}")
                     test_ind += 1
-                    df_sol, tot_cost, time_spent = self.solver(
+                    df_sol, tot_cost, pattern_list, time_spent = self.solver(
                         df_items, df_vehicles, time_limit
                     )
                     list_cost.append(tot_cost)
@@ -196,6 +196,7 @@ class SolverACO:
         weightRatio = 0
         i = 0
         id_prev_truck = None
+        pattern_list = []
         # Loop until there are no more items left
         while more_items:
             # Decision for the most convenient vehicle
@@ -231,8 +232,21 @@ class SolverACO:
                 print("Truck: ", self.id_vehicle)
 
             # Method to solve the 2D bin packing problem
-            bestAnts = aco.aco_2D_bin(last_iter=last_iter)
+            bestAnts, bestAreas = aco.aco_2D_bin(last_iter=last_iter)
             bestAnt = bestAnts[0]
+
+            column = np.zeros(len(stackInfo))
+            for stack in bestAnt:
+                # TODO: use code to state
+                column[stack.stack_code] += len(stack.items)
+
+            pattern_list.append(
+                {
+                    "pattern": column,
+                    "vehicle": vehicle.id_truck,
+                    "area": bestAreas[0],
+                }
+            )
 
             self.solUpdate(bestAnt, vehicle)
 
@@ -276,7 +290,7 @@ class SolverACO:
         print(f"\nTime: {time_spent} s")
 
         # Return the dataframe solution and its cost to check the best solution among all the iteration
-        return df_sol, totCost, time_spent
+        return df_sol, totCost, pattern_list, time_spent
 
     def buildStacks(self, vehicle, df_items, stackInfo):
         """
