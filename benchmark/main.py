5d6b0d326e951c6ee9a54dfb84dd460aa3e06470
Alessandro Redi <alessandroredi1@gmail.com>
2023-11-10 09:58:26 +0100

    Solution creation (for checker)

--- benchmark/main.py
+++ benchmark/main.py
@@ -6,7 +6,7 @@ import pandas as pd
 from columnGeneration import columnGeneration
 
 if __name__ == "__main__":
-    dataset_name = "datasetA"
+    dataset_name = "dataset_small"
     sol_file_name = f"{dataset_name}_"
     df_items = pd.read_csv(
         os.path.join(".", "data", dataset_name, "items.csv"),
@@ -15,6 +15,6 @@ if __name__ == "__main__":
         os.path.join(".", "data", dataset_name, "vehicles.csv"),
     )
 
-    # df_vehicles = df_vehicles.iloc[[1, 6]]
+    df_vehicles = df_vehicles.iloc[[1, 2]]
     model_cg = columnGeneration()
     bestAnts = model_cg.solve(df_items, df_vehicles)
