cfd867926d09ec5c0a81ea72d887f3645cf3831d
davmacario <dav.macario@gmail.com>
2024-01-25 18:51:57 -0600

    [fix]: solve import issues

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
+from .aco import SolverACO
+from .aco_master import masterAco
 from .column_generation import columnGeneration
 from .ortools import solverORTools
-from .aco_master import masterAco
-from .aco import SolverACO
-
-# from .aco import *
-# from .exact import *
 
 __all__ = ["columnGeneration", "solverORTools", "masterAco", "SolverACO"]
