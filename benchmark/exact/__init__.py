a5d8e363d2fb9d07ea4701c9fd85dcdb9dbf6419
davmacario <dav.macario@gmail.com>
2024-01-25 20:41:43 -0600

    [fix]: review __init__.py files and solve import errors

--- benchmark/exact/__init__.py
+++ benchmark/exact/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from exact.solve_exact import ExactSolver
-from exact.check.check3D_file import check_above, check_3D
+from .solve_exact import ExactSolver
 
-__all__ = ["ExactSolver", "check_above", "check_3D"]
+__all__ = ["ExactSolver"]
