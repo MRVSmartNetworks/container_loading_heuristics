5037da1974d6e53fcd3a97a1ef3e2d79d01a25e7
federicovolponi <federicovolponi99@gmail.com>
2024-11-28 14:42:32 +0100

    script for sample datasets

---
+++ scripts/sample_datasets.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+import random
+
+rootDir = "data"
+for subdir, dirs, files in os.walk(rootDir):
+    if subdir.startswith(rootDir + "/dataset"):
+        newDatasetsDir = [subdir + str(i + 1) for i in range(5)]
+        items = pd.read_csv(subdir + "/items.csv")
+        vehicles = pd.read_csv(subdir + "/vehicles.csv")
+        for newDir in newDatasetsDir:
+            if not os.path.exists(newDir):
+                os.makedirs(newDir)
+            sampledItems = items.sample(frac=random.uniform(0.3, 0.5))
+            sampledItems.to_csv(newDir + "/items.csv")
+            vehicles.to_csv(newDir + "/vehicles.csv")
+            print("New dataset in", newDir)
+
+
+
